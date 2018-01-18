079008e7f67133b1d6a59f23e79f502b730b3797
Daniel Pino <pinonium@Daniels-MacBook-Pro-2.local>
2018-01-18 10:37:35 -0500

    added userProfile route and components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Home from './components/Home'
+import UserProfile from './components/UserProfile'
 
 class App extends Component {
   state = {
-    accountBalance: 14568.27
+    accountBalance: 14568.27,
+    currentUser: {
+      userName: 'bob_loblaw',
+      memberSince: '08/23/99'
+    }
   }
 
   render() {
@@ -13,14 +18,18 @@ class App extends Component {
         accountBalance={this.state.accountBalance} />
     )
 
+    const UserProfileComponent = () => (
+      <UserProfile
+        userName={this.state.currentUser.userName}
+        memberSince={this.state.currentUser.memberSince} />
+    )
+
 
     return (
       <Router>
         <Switch>
-          <Route
-            exact
-            path="/"
-            render={HomeComponent} />
+          <Route exact path="/" render={HomeComponent} />
+          <Route exact path="/userProfile" render={UserProfileComponent} />
         </Switch>
       </Router>
     );
